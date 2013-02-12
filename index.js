152b64b5978738b6c720714c3f6fc3f067235744
freewil <sean@eternalrise.com>
2013-02-12 16:15:00 -0500

    use strict equivalency

--- index.js
+++ index.js
@@ -1,10 +1,18 @@
+/**
+ * Does a constant-time string comparison by not short-circuiting
+ * on first sign of non-equivalency.
+ * 
+ * @param {String} a The first string to be compared against the second
+ * @param {String} b The second string to be compared against the first 
+ * @return {Boolean}
+ */
 module.exports = function scmp(a, b) {
-  if (a.length != b.length) {
+  if (a.length !== b.length) {
     return false;
   }
   var result = 0;
   for (var i = 0; i < a.length; ++i) {
     result |= a.charCodeAt(i) ^ b.charCodeAt(i);
   }
-  return result == 0;
+  return result === 0;
 };
--- test/test.js
+++ test/test.js
@@ -5,10 +5,12 @@ describe('scmp', function() {
   it('should return true for identical strings', function() {
     assert(scmp('a', 'a'));
     assert(scmp('abc', 'abc'));
+    assert(scmp('e727d1464ae12436e899a726da5b2f11d8381b26', 'e727d1464ae12436e899a726da5b2f11d8381b26'));
   });
   
   it('should return false for non-identical strings', function() {
     assert.ifError(scmp('a', 'b'));
     assert.ifError(scmp('abc', 'b'));
+    assert.ifError(scmp('e727d1464ae12436e899a726da5b2f11d8381b26', 'e727e1b80e448a213b392049888111e1779a52db'));
   });
 });
