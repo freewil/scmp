11d48e2d4bb0b47fa05dae1d9356fc6fa353edf5
Sean Lavine <freewil@users.noreply.github.com>
2016-11-05 16:45:34 -0700

    use Buffers instead of strings

--- benchmark/benchmark.js
+++ benchmark/benchmark.js
@@ -1,15 +1,19 @@
 var scmp = require('../');
 
+// use safe-buffer in case Buffer.from in newer versions of node aren't
+// available
+var Buffer = require('safe-buffer').Buffer;
+
 suite('scmp', function() {
-  var HASH1 = 'e727d1464ae12436e899a726da5b2f11d8381b26';
-  var HASH2 = 'f727d1464ae12436e899a726da5b2f11d8381b26';
-  
+  var HASH1 = Buffer.from('e727d1464ae12436e899a726da5b2f11d8381b26', 'utf8');
+  var HASH2 = Buffer.from('f727d1464ae12436e899a726da5b2f11d8381b26', 'utf8');
+
   bench('short-circuit compares', function() {
     HASH1 === HASH2;
   });
-  
+
   bench('scmp compares', function() {
     scmp(HASH1, HASH2);
   });
-  
+
 });
