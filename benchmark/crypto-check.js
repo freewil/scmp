6a8f4dd764792c1f033432a2523c97e4c8df9200
Sean Lavine <freewil@users.noreply.github.com>
2016-12-30 14:27:22 -0500

    add benchmark for crypto.timingSafeEqual check

--- benchmark/crypto-check.js
+++ benchmark/crypto-check.js
@@ -1,5 +1,8 @@
+var crypto = require('crypto');
 var Benchmark = require('benchmark');
-var scmp = require('../');
+
+var scmpCompare = require('../lib/scmpCompare');
+var compareFn = crypto.timingSafeEqual || scmpCompare;
 
 // `safe-buffer` in case `Buffer.from` in newer versions of node aren't available
 var Buffer = require('safe-buffer').Buffer;
@@ -8,11 +11,14 @@ var HASH1 = Buffer.from('e727d1464ae12436e899a726da5b2f11d8381b26', 'hex');
 var HASH2 = Buffer.from('f727d1464ae12436e899a726da5b2f11d8381b26', 'hex');
 
 var suite = new Benchmark.Suite();
-suite.add('short-circuit compares', function() {
-  HASH1 === HASH2;
+suite.add('crypto check each fn call', function() {
+  if (crypto.timingSafeEqual) {
+    return crypto.timingSafeEqual(HASH1, HASH2);
+  }
+  return scmpCompare(HASH1, HASH2);
 })
-.add('scmp compares', function() {
-  scmp(HASH1, HASH2);
+.add('crypto check once', function() {
+  return compareFn(HASH1, HASH2);
 })
 .on('cycle', function(event) {
   console.log(String(event.target));
