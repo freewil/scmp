0d55f28dff5bf10c9c370b2117906ab3e354a3a7
Sean Lavine <freewil@users.noreply.github.com>
2016-12-30 13:45:09 -0500

    replace matcha with benchmark

--- benchmark/benchmark.js
+++ benchmark/benchmark.js
@@ -1,19 +1,20 @@
+var Benchmark = require('benchmark');
 var scmp = require('../');
 
-// use safe-buffer in case Buffer.from in newer versions of node aren't
-// available
+// `safe-buffer` in case `Buffer.from` in newer versions of node aren't available
 var Buffer = require('safe-buffer').Buffer;
 
-suite('scmp', function() {
-  var HASH1 = Buffer.from('e727d1464ae12436e899a726da5b2f11d8381b26', 'hex');
-  var HASH2 = Buffer.from('f727d1464ae12436e899a726da5b2f11d8381b26', 'hex');
+var HASH1 = Buffer.from('e727d1464ae12436e899a726da5b2f11d8381b26', 'hex');
+var HASH2 = Buffer.from('f727d1464ae12436e899a726da5b2f11d8381b26', 'hex');
 
-  bench('short-circuit compares', function() {
-    HASH1 === HASH2;
-  });
-
-  bench('scmp compares', function() {
-    scmp(HASH1, HASH2);
-  });
-
-});
+var suite = new Benchmark.Suite();
+suite.add('short-circuit compares', function() {
+  HASH1 === HASH2;
+})
+.add('scmp compares', function() {
+  scmp(HASH1, HASH2);
+})
+.on('cycle', function(event) {
+  console.log(String(event.target));
+})
+.run();
