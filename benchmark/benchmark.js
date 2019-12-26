03700088fcdddb2b219d7bd58a8ee3df7183c74c
Sean Lavine <freewil@users.noreply.github.com>
2019-12-26 13:49:41 -0500

    standard --fix

--- benchmark/benchmark.js
+++ benchmark/benchmark.js
@@ -1,23 +1,23 @@
-var Benchmark = require('benchmark');
-var scmp = require('../');
+var Benchmark = require('benchmark')
+var scmp = require('../')
 
 // `safe-buffer` in case `Buffer.from` in newer versions of node aren't available
-var Buffer = require('safe-buffer').Buffer;
+var Buffer = require('safe-buffer').Buffer
 
-var HASH1 = Buffer.from('e727d1464ae12436e899a726da5b2f11d8381b26', 'hex');
-var HASH2 = Buffer.from('f727d1464ae12436e899a726da5b2f11d8381b26', 'hex');
+var HASH1 = Buffer.from('e727d1464ae12436e899a726da5b2f11d8381b26', 'hex')
+var HASH2 = Buffer.from('f727d1464ae12436e899a726da5b2f11d8381b26', 'hex')
 
-var suite = new Benchmark.Suite();
-suite.add('short-circuit compares', function() {
-  HASH1 === HASH2;
+var suite = new Benchmark.Suite()
+suite.add('short-circuit compares', function () {
+  HASH1 === HASH2
 })
-.add('scmp compares', function() {
-  scmp(HASH1, HASH2);
-})
-.on('cycle', function(event) {
-  console.log(String(event.target));
-})
-.on('complete', function() {
-  console.log('Fastest is ' + this.filter('fastest').map('name'));
-})
-.run();
+  .add('scmp compares', function () {
+    scmp(HASH1, HASH2)
+  })
+  .on('cycle', function (event) {
+    console.log(String(event.target))
+  })
+  .on('complete', function () {
+    console.log('Fastest is ' + this.filter('fastest').map('name'))
+  })
+  .run()
--- benchmark/crypto-check.js
+++ benchmark/crypto-check.js
@@ -1,29 +1,29 @@
-var crypto = require('crypto');
-var Benchmark = require('benchmark');
+var crypto = require('crypto')
+var Benchmark = require('benchmark')
 
-var scmpCompare = require('../lib/scmpCompare');
-var compareFn = crypto.timingSafeEqual || scmpCompare;
+var scmpCompare = require('../lib/scmpCompare')
+var compareFn = crypto.timingSafeEqual || scmpCompare
 
 // `safe-buffer` in case `Buffer.from` in newer versions of node aren't available
-var Buffer = require('safe-buffer').Buffer;
+var Buffer = require('safe-buffer').Buffer
 
-var HASH1 = Buffer.from('e727d1464ae12436e899a726da5b2f11d8381b26', 'hex');
-var HASH2 = Buffer.from('f727d1464ae12436e899a726da5b2f11d8381b26', 'hex');
+var HASH1 = Buffer.from('e727d1464ae12436e899a726da5b2f11d8381b26', 'hex')
+var HASH2 = Buffer.from('f727d1464ae12436e899a726da5b2f11d8381b26', 'hex')
 
-var suite = new Benchmark.Suite();
-suite.add('crypto check each fn call', function() {
+var suite = new Benchmark.Suite()
+suite.add('crypto check each fn call', function () {
   if (crypto.timingSafeEqual) {
-    return crypto.timingSafeEqual(HASH1, HASH2);
+    return crypto.timingSafeEqual(HASH1, HASH2)
   }
-  return scmpCompare(HASH1, HASH2);
+  return scmpCompare(HASH1, HASH2)
 })
-.add('crypto check once', function() {
-  return compareFn(HASH1, HASH2);
-})
-.on('cycle', function(event) {
-  console.log(String(event.target));
-})
-.on('complete', function() {
-  console.log('Fastest is ' + this.filter('fastest').map('name'));
-})
-.run();
+  .add('crypto check once', function () {
+    return compareFn(HASH1, HASH2)
+  })
+  .on('cycle', function (event) {
+    console.log(String(event.target))
+  })
+  .on('complete', function () {
+    console.log('Fastest is ' + this.filter('fastest').map('name'))
+  })
+  .run()
